e3f37fd1fdc8301c383680da9d14d5aac13a5558
PhamMinhHiep <hieppm@vingroup.net>
2022-01-03 23:47:25 +0700

    update fire detection

--- custom.py
+++ custom.py
@@ -1,54 +1,67 @@
-from imageai.Detection.Custom import CustomVideoObjectDetection
 import os
 import cv2
+from imageai.Detection.Custom import (
+    CustomVideoObjectDetection,
+    CustomObjectDetection
+)
+
 
 execution_path = os.getcwd()
 
 
 class RealtimeVideoObjectDetection(CustomVideoObjectDetection):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, model_path, json_path):
+        self.__detector = CustomObjectDetection()
+        self.__detector.setModelTypeAsYOLOv3()
+        self.__detector.setModelPath(detection_model_path=model_path)
+        self.__detector.setJsonPath(configuration_json=json_path)
+        self.__detector.loadModel()
 
-    def detectObjectsFromVideo(self,
-                               minimum_percentage_probability=50,
-                               display_percentage_probability=True,
-                               display_object_name=True
-                               ):
-
+    def detect_from_camera(self, frame_detection_interval=1, minimum_percentage_probability=50,
+                           display_percentage_probability=True, display_object_name=True):
+        """
+        Detect fire with input from webcam
+        """
         input_video = cv2.VideoCapture(0)
+        counting = 0
 
         video_frames_count = 0
 
-        while (True):
-            _, frame = input_video.read()
-            print(frame)
-            detected_frame = frame.copy()
-            video_frames_count += 1
-            try:
-                detected_frame, _ = self.__detector.detectObjectsFromImage(
-                    input_image=frame, input_type="array", output_type="array",
-                    minimum_percentage_probability=minimum_percentage_probability,
-                    display_percentage_probability=display_percentage_probability,
-                    display_object_name=display_object_name)
-            except:
-                None
-            cv2.imshow('frame', detected_frame)
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
+        while (input_video.isOpened()):
+            ret, frame = input_video.read()
+            if (ret == True):
+                detected_frame = frame.copy()
+                video_frames_count += 1
+                counting += 1
 
-            input_video.release()
+                check_frame_interval = counting % frame_detection_interval
+
+                if (counting == 1 or check_frame_interval == 0):
+                    try:
+                        detected_frame, _ = self.__detector.detectObjectsFromImage(
+                            input_image=frame, input_type="array", output_type="array",
+                            minimum_percentage_probability=minimum_percentage_probability,
+                            display_percentage_probability=display_percentage_probability,
+                            display_object_name=display_object_name)
+                    except Exception as e:
+                        print(e)
+                        None
+                cv2.imshow("frame", detected_frame)
+
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break
+
+        input_video.release()
 
 
-def detect_from_camera():
-    detector = RealtimeVideoObjectDetection()
-    detector.setModelTypeAsYOLOv3()
-    detector.setModelPath(detection_model_path=os.path.join(
-        execution_path, "detection_model-ex-33--loss-4.97.h5"))
-    detector.setJsonPath(configuration_json=os.path.join(
-        execution_path, "detection_config.json"))
-    detector.loadModel()
-    detector.detectObjectsFromVideo()
+def fire_detection():
+    model_path = os.path.join(
+        execution_path, "detection_model-ex-33--loss-4.97.h5")
+    json_path = os.path.join(execution_path, "detection_config.json")
+    detector = RealtimeVideoObjectDetection(
+        model_path=model_path, json_path=json_path)
+    detector.detect_from_camera()
 
 
 if __name__ == "__main__":
-    detect_from_camera()
+    fire_detection()
